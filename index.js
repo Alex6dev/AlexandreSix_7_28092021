f9a3f2ca4fbfaee31036b15a3ec5ae0bcd25d30c
Alexandre SIX <alexandresix2@gmail.com>
2021-10-01 13:54:02 +0200

    html ok css ok js en cours, class recette et vignette filtre ok

--- index.js
+++ index.js
@@ -1,107 +1,4 @@
-function enTete(){
-    const element=document.createElement("header");
-    element.innerHTML=`
-    <a href="index.html"><img src="logo.png" aria-label="lien vers accueil" id="logo"/></a>
-    `;
-    document
-        .getElementById('app')
-        .appendChild(element)
-    
-}
-enTete();
-
-function main(){
-    const element= document.createElement("main");
-    element.innerHTML=`
-    <form name="search" role="search" method="get" action="" id="form" onsubmit="return validate();">
-        <div id="boxGrandInput">
-            <label for="recherche"></label>
-            <input type="text" id="recherche" name="recherche" placeholder="Rechercher un ingrédient, appareil, ustensiles ou recette "></input>
-            <i class="fas fa-search loupe"></i>
-        </div>
-        <div id="boxTagsActifs">
-            <div class="etiquette Ing" aria-label="étiquette filtre actif"><p>coco</p><i class="far fa-times-circle croix"></i></div>
-            <div class="etiquette App" aria-label="étiquette filtre actif"><p>fouet</p><i class="far fa-times-circle croix"></i></div>
-            <div class="etiquette Ust" aria-label="étiquette filtre actif"><p>cuiellere</p><i class="far fa-times-circle croix"></i></div>
-        </div>
-        <section id="boxZoneDesInputs">
-            <label for="ingredients" ></label>
-            <input type="text" class="Ing" id="ingredients" name="ingredients" placeholder="Ingredients"></input>
-            <i class="fas fa-chevron-up fa-chevron-upIng"></i>
-            <table id="tabIng">
-                <tbody>
-                    <tr>
-                        <td>Lait de coco</td>
-                        <td>Jus de citron</td>
-                        <td>Crème de coco</td>
-                    </tr>
-                    <tr>
-                        <td>Sucre</td>
-                        <td>Glaçons</td>
-                        <td>Thon Rouge</td>
-                    </tr>
-                    <tr>
-                        <td>Concombre</td>
-                        <td>Tomate</td>
-                        <td>Carotte</td>
-                    </tr>
-                    <tr>
-                        <td>Citron Vert</td>
-                        <td>Poulet</td>
-                        <td>Coulis de tomate</td>
-                    </tr>
-                    <tr>
-                        <td>Oignon</td>
-                        <td>Poivron rouge</td>
-                        <td>Huile d'olive</td>
-                    </tr>
-                    <tr>
-                        <td>Riz blanc</td>
-                        <td>Thon en miettes</td>
-                        <td>Oeuf dur</td>
-                    </tr>
-                    <tr>
-                        <td>Maïs</td>
-                        <td>Vinaigrette</td>
-                        <td>Pâte feuilletée</td>
-                    </tr>
-                    <tr>
-                        <td>Crème fraiche</td>
-                        <td>Gruyère râpé</td>
-                        <td>Moutarde de dijon</td>
-                    </tr>
-                    <tr>
-                        <td>Pomme</td>
-                        <td>Oeuf</td>
-                        <td>Sucre en Poudre</td>
-                    </tr>
-                    <tr>
-                        <td>Chocolat au lait</td>
-                        <td>Crème liquide</td>
-                        <td>Beurre</td>
-                    </tr>
-                </tbody>
-            </table>
-            <label for="appareil" ></label>
-            <input type="text" class="App" id="appareil" name="appareil" placeholder="Appareil"></input>
-            <i class="fas fa-chevron-up fa-chevron-upApp"></i>
-            <label for="ustensiles" ></label>
-            <input type="text" class="Ust" id="ustensiles" name="ustensiles" placeholder="Ustensiles"></input>
-            <i class="fas fa-chevron-up fa-chevron-upUst"></i>    
-        </section>
-    </form>
-    <section id="listeRecette">
-        <div id="boxRecette">
-        </div>
-    </section>
-    `;
-    document
-        .getElementById("app")
-        .appendChild(element)
-}
-main()
-
-const recipes = [ 
+/*const recipes = [ 
     {
         "id": 1,
         "name" : "Limonade de Coco",
@@ -207,8 +104,100 @@ const recipes = [
         "appliance": "Cocotte",
         "ustensils": ["couteau"]
     }
-]
-console.log(recipes)
+]*/
+
+import { recipes } from "./recipes.js";
+
+  
+  
+function enTete(){
+    const element=document.createElement("header");
+    element.innerHTML=`
+    <a href="index.html"><img src="logo.png" aria-label="lien vers accueil" id="logo"/></a>
+    `;
+    document
+        .getElementById('app')
+        .appendChild(element)
+    
+}
+enTete();
+
+function main(){
+    console.log(recipes)
+    const element= document.createElement("main");
+    element.innerHTML=`
+    <form name="search" role="search" method="get" action="" id="form" onsubmit="return validate();">
+        <div id="boxGrandInput">
+            <label for="recherche"></label>
+            <input type="text" id="recherche" name="recherche" placeholder="Rechercher un ingrédient, appareil, ustensiles ou recette "></input>
+            <i class="fas fa-search loupe"></i>
+        </div>
+        <div id="boxTagsActifs">
+        
+        </div>
+        <section id="boxZoneDesInputs">
+            <label for="ingredients" ></label>
+            <input type="text" class="Ing" id="ingredients" name="ingredients" placeholder="Ingredients"></input>
+            <i class="fas fa-chevron-up fa-chevron-upIng"></i>
+            
+            <label for="appareil" ></label>
+            <input type="text" class="App" id="appareil" name="appareil" placeholder="Appareil"></input>
+            <i class="fas fa-chevron-up fa-chevron-upApp"></i>
+            <label for="ustensiles" ></label>
+            <input type="text" class="Ust" id="ustensiles" name="ustensiles" placeholder="Ustensiles"></input>
+            <i class="fas fa-chevron-up fa-chevron-upUst"></i>    
+        </section>
+    </form>
+    <section id="listeRecette">
+        <div id="boxRecette">
+        </div>
+    </section>
+    `;
+    document
+        .getElementById("app")
+        .appendChild(element)
+    
+}
+main()
+document.getElementById("ingredients").addEventListener("input",displayVignetteIng)
+function displayVignetteIng(e){
+    e.preveutDefault();
+    let evtt=e.target.value
+    const regex=new RegExp('[a-zA-Z]{3,}')
+    if(regex.test(evtt)){
+        new vignetteIng(evtt)
+    } 
+}
+
+class vignetteIng{
+    constructor(nom){
+        this.element=this.displayVignette(nom)
+        document.getElementById("boxTagsActifs").appendChild(this.element)
+        document.querySelector('#'+nom+' .croix').addEventListener('click',()=>this.removeVignette(nom)) 
+    }
+removeVignette(nom){
+    let vignetteCibleRemove=document.getElementById(nom)
+    document.getElementById("boxTagsActifs").removeChild(vignetteCibleRemove) 
+}
+displayVignette(nom){
+    let elt=document.createElement("div");
+    elt.setAttribute("class","etiquette")
+    elt.setAttribute("id",nom)
+    elt.classList.add("Ing") 
+    elt.setAttribute("aria-label","étiquette filtre actif")
+    elt.innerHTML=`
+        <p>${nom}</p>
+        <i class="far fa-times-circle croix"></i>
+    `;
+    return elt;
+}
+
+}
+/*
+<div class="etiquette Ing" aria-label="étiquette filtre actif"><p>coco</p><i class="far fa-times-circle croix"></i></div>
+            <div class="etiquette App" aria-label="étiquette filtre actif"><p>fouet</p><i class="far fa-times-circle croix"></i></div>
+            <div class="etiquette Ust" aria-label="étiquette filtre actif"><p>cuiellere</p><i class="far fa-times-circle croix"></i></div>
+*/        
 class recette{
     constructor(element){
         this.element=this.displayRecette(element)
@@ -252,7 +241,7 @@ class recette{
                     
                 </div>
                 <div class="recette__Proces">
-                    <p>${element.description}</p>
+                    <p class="recette__descrip">${element.description}</p>
                 </div>
             </div>
         </div>    
@@ -262,41 +251,61 @@ class recette{
     }
 }
 recipes.forEach(element=>{new recette(element)})   
-/*<div class="recette">
-            <div class="recette__Vide"></div>
-            <div class="recette__Info">
-                <div class="recette__TitreTemps">
-                    <p class="recette__Titre">limodade de coco</p>
-                    <div class="recette__BoxTemps">
-                        <i class="far fa-clock recette__clock"></i>
-                        <p class="recette__Temps">60m</p>
-                    </div>
-                </div>
-                <div class="recette__IngProces">
-                    <div class="recette__Ingredients">
-                        <p>lait de coco: 400ml</p>
-                        <p>jus de citron: 2</p>
-                        <p>créme de coco: 4cuillères</p>
-                        <p>sucre: 20g</p>
-                        <p>glaçons: 2</p>
-                    </div>
-                    <div class="recette__Proces">
-                        <p>Mettre les glaçons à votre goût dans le blender, ajouter le lait, la crème de coco, le jus de 2 citrons et le sucre. Mixer jusqu'à avoir la consistence désirée</p>
-                    </div>
-                </div>
-            </div>
-        </div> */
-/*import { recipes } from "./recipes";
-console.log(recipes)
-import("./recipes.js")
-    .then(function(res){
-        if(res.ok){
-            return res.json();
-        }
-    })
-    .then(function (module){
-        console.log(module)
-    })
-    .catch(function(err){
-        console.error(err);
-    })*/
+
+
+
+
+    /*<table id="tabIng">
+                <tbody>
+                    <tr>
+                        <td>Lait de coco</td>
+                        <td>Jus de citron</td>
+                        <td>Crème de coco</td>
+                    </tr>
+                    <tr>
+                        <td>Sucre</td>
+                        <td>Glaçons</td>
+                        <td>Thon Rouge</td>
+                    </tr>
+                    <tr>
+                        <td>Concombre</td>
+                        <td>Tomate</td>
+                        <td>Carotte</td>
+                    </tr>
+                    <tr>
+                        <td>Citron Vert</td>
+                        <td>Poulet</td>
+                        <td>Coulis de tomate</td>
+                    </tr>
+                    <tr>
+                        <td>Oignon</td>
+                        <td>Poivron rouge</td>
+                        <td>Huile d'olive</td>
+                    </tr>
+                    <tr>
+                        <td>Riz blanc</td>
+                        <td>Thon en miettes</td>
+                        <td>Oeuf dur</td>
+                    </tr>
+                    <tr>
+                        <td>Maïs</td>
+                        <td>Vinaigrette</td>
+                        <td>Pâte feuilletée</td>
+                    </tr>
+                    <tr>
+                        <td>Crème fraiche</td>
+                        <td>Gruyère râpé</td>
+                        <td>Moutarde de dijon</td>
+                    </tr>
+                    <tr>
+                        <td>Pomme</td>
+                        <td>Oeuf</td>
+                        <td>Sucre en Poudre</td>
+                    </tr>
+                    <tr>
+                        <td>Chocolat au lait</td>
+                        <td>Crème liquide</td>
+                        <td>Beurre</td>
+                    </tr>
+                </tbody>
+            </table> */
